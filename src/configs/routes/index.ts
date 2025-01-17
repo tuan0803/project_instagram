312cddacb1321cb449a6cb08f5c5842fa10ae5ff
tuan080321 <nvt08032k1@gmail.com>
2025-01-17 19:06:51 +0700

    Login

--- src/configs/routes/index.ts
+++ src/configs/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 // import UserRoute from './Users';
 import Register from './Register';
+import Login from './Login';
 
 const router = Router();
 
 // router.use('/users', userPassport.authenticate('jwt', { session: false }), UserRoute);
 router.use('/register', Register);
+router.use('/login', Login);
 
 export default router;
