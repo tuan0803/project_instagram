d77d435497d09aae6b54d554fab9c5bec7b477f7
Kravys <kiencuongthanh@gmail.com>
2025-01-20 09:41:23 +0700

    Chức năng theo dõi

--- src/configs/routes/FollowerRoutes.ts
+++ src/configs/routes/FollowerRoutes.ts
@@ -3,14 +3,6 @@ import FollowerController from '@controllers/api/Follower';
 
 const router = Router();
 
-router.post('/follow1', FollowerController.follow);
-
-router.post('/approveFollow/:followId', FollowerController.approveFollow);
-
-router.delete('/unfollow/:followeeId', FollowerController.unfollow);
-
-router.get('/followers', FollowerController.followersList);
-
-router.get('/following', FollowerController.followingList);
+router.post('/users/:userId/follow', FollowerController.follow);
 
 export default router;
--- src/models/followers.ts
+++ src/models/followers.ts
@@ -1,6 +1,7 @@
 import { Model, ModelScopeOptions, Sequelize } from 'sequelize';
 import FollowerEntity from '@entities/followers';
 import FollowerInterface from '@interfaces/followers';
+import NotificationModel from '@models/notifications';
 // import { ModelHooks } from 'sequelize/types/lib/hooks';
 
 class FollowerModel extends Model<FollowerInterface> implements FollowerInterface {
@@ -44,7 +45,30 @@ class FollowerModel extends Model<FollowerInterface> implements FollowerInterfac
 
   public static initialize (sequelize: Sequelize) {
     this.init(FollowerEntity, {
-      // hooks: FollowerModel.hooks,
+      hooks: {
+        async beforeCreate (follower) {
+          const existingFollower = await FollowerModel.scope([
+            { method: ['byFollowerAndFollowee', follower.followerId, follower.followeeId] },
+          ]).findOne();
+          if (existingFollower) {
+            throw new Error('Bạn đã theo dõi người này');
+          }
+        },
+        async afterCreate (follower) {
+          const mockUser = {
+            name: 'Test User',
+          };
+          await NotificationModel.create({
+            userId: follower.followerId,
+            notifiableType: 'follow_request',
+            notifiableId: follower.id,
+            title: 'Yêu cầu theo dõi mới',
+            shortContent: `${mockUser.name} đã gửi yêu cầu theo dõi bạn.`,
+            content: `Người dùng ${mockUser.name} muốn theo dõi bạn.`,
+          });
+        },
+      },
+
       scopes: FollowerModel.scopes,
       tableName: 'followers',
       sequelize,
