a2f4f540491afe7a2af82c51ffcbfc44328645d2
tuan080321 <nvt08032k1@gmail.com>
2025-01-20 11:08:26 +0700

    Add Register

---
+++ src/configs/routes/Register.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+import registerController from '@controllers/auth/Register';
+import strongParams from '@middlewares/parameters'; 
+const router = express.Router();
+
+router.post('/', strongParams(), registerController.register);
+
+export default router;
