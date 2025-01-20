ea8be4b00086e517dac19692f613cd32d9beaf3d
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-01-20 16:33:35 +0700

    model

---
+++ src/models/posts.ts
@@ -0,0 +1,50 @@
+import { Model, Sequelize, ModelScopeOptions } from 'sequelize';
+import PostEntity from '@entities/posts';
+import PostInterface, { PostCreationAttributes } from '@interfaces/posts';
+import { ModelHooks } from 'sequelize/types/lib/hooks';
+
+class PostModel extends Model<PostInterface, PostCreationAttributes> implements PostInterface {
+  public id: number;
+  public userId: number;
+  public text: string;
+  public createdAt?: Date;
+  public updatedAt?: Date;
+  public commentCount: number;
+  public reactionCount: number;
+
+  static readonly NOTIFIABLE_TYPE_ENUM = {
+    SYSTEM: 'system',
+  };
+
+  static readonly hooks: Partial<ModelHooks<PostModel>> = {
+    async beforeCreate (record) {
+      record.commentCount = record.commentCount || 0;
+      record.reactionCount = record.reactionCount || 0;
+    },
+    async afterCreate (record) {
+      console.log('Done post:', record);
+    },
+  };
+
+  static readonly scopes: ModelScopeOptions = {
+    byId (id) {
+      return {
+        where: { id },
+      };
+    },
+  };
+
+  public static initialize (sequelize: Sequelize) {
+    this.init(PostEntity, {
+      hooks: PostModel.hooks,
+      tableName: 'posts',
+      sequelize,
+      timestamps: true,
+    });
+  }
+
+  public static associate () {
+  }
+}
+
+export default PostModel;
