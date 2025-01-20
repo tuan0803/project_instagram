0b6bd463e68b55aba7b2ba0c53fc6caf8887cc8f
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-01-20 16:33:09 +0700

    interface

---
+++ src/interfaces/posts.ts
@@ -0,0 +1,50 @@
+import { DateTimeUtil } from '@libs/datetime';
+import { DataTypes } from 'sequelize';
+
+const PostEntity = {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'user_id',
+  },
+  text: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+    field: 'text',
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'created_at',
+    get (): number {
+      return this.getDataValue('created_at') ? DateTimeUtil.dateToMs(this.getDataValue('created_at')) : null;
+    },
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    field: 'updated_at',
+    get (): number {
+      return this.getDataValue('updated_at')
+        ? DateTimeUtil.dateToMs(this.getDataValue('updated_at'))
+        : null;
+    },
+  },
+  commentCount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'comment_count',
+  },
+  reactionCount: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'reaction_count',
+  },
+};
+export default PostEntity;
