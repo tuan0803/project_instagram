a1b664e90ba60bf998697068754668021f6f8510
Kravys <kiencuongthanh@gmail.com>
2025-01-21 09:48:42 +0700

    Fix phần, add Migration

--- src/controllers/api/Follower.ts
+++ src/controllers/api/Follower.ts
@@ -2,16 +2,19 @@ import { Request, Response } from 'express';
 import { sendError, sendSuccess } from '@libs/response';
 // import { NoData } from '@libs/errors';
 import FollowerModel from '@models/followers';
-
+import UserModel from '@models/users';
 class FollowerController {
   public async follow (req: Request, res: Response) {
     try {
       const { followeeId } = req.body;
-      // const targetUser = await UserModel.findByPk(followeeId);// Chưa có UserModel
+      const targetUser = await UserModel.findByPk(followeeId);
 
-      // if (!targetUser) {
-      //   return sendError(res, 404, 'Người dùng không tồn tại.');
-      // }
+      if (!targetUser) {
+        return sendError(res, 404, 'Người dùng không tồn tại.');
+      }
+      if (targetUser.isPrivate){
+        return sendSuccess (res, {}, 'Tài khoản Private, cần đợi xác nhận')
+      }
       // User ảo
       const mockUser = {
         'x-user-id': 1,
@@ -22,10 +25,10 @@ class FollowerController {
       const newFollow = await FollowerModel.create({
         followerId,
         followeeId,
-        isApproved: false,
+        isApproved: true,
       });
 
-      sendSuccess(res, { item: newFollow }, 'Yêu cầu theo dõi đã được gửi.');
+      sendSuccess(res, { item: newFollow }, 'Bắt đầu theo dõi người dùng này.');
     } catch (error) {
       sendError(res, 500, error.message, error);
     }
