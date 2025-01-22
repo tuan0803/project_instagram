3bb5e09709ce208bce919ec85e29f135abab3126
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-01-22 21:44:20 +0700

    Fix bug

--- src/interfaces/notifications.ts
+++ src/interfaces/notifications.ts
@@ -1,16 +1,16 @@
-// interface NotificationInterface {
-//   id: number;
-//   userId: number;
-//   deviceToken?: string;
-//   notifiableType: string;
-//   notifiableId: number;
-//   title: string;
-//   shortContent: string;
-//   content: string;
-//   readAt?: number;
-//   data?: any;
-//   createdAt?: Date;
-//   updatedAt?: Date;
-// };
+interface NotificationInterface {
+  id: number;
+  userId: number;
+  deviceToken?: string;
+  notifiableType: string;
+  notifiableId: number;
+  title: string;
+  shortContent: string;
+  content: string;
+  readAt?: number;
+  data?: any;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
 
-// export default NotificationInterface;
+export default NotificationInterface;
