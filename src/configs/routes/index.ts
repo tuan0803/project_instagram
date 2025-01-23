9d988957da0fe7f2015a4e110e5f8af595b302e7
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-01-23 16:19:07 +0700

    thay doi trong file

--- src/configs/routes/index.ts
+++ src/configs/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import PostRouter from '@controllers/api/Posts';
+import PostRouter from '@controllers/Posts';
 
 const router = Router();
 
+router.get('/posts/userId/:userId', PostRouter.getAllPosts);
 router.post('/posts', PostRouter.create);
+router.put('/posts/update/userId/:id', PostRouter.update);
+router.delete('/posts/delete/:id', PostRouter.delete);
 
 export default router;
