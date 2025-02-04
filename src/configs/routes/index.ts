1ac3fe8d17121605842b697255c34f6f8702fe91
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-02-04 17:26:07 +0700

    post index

--- src/configs/routes/index.ts
+++ src/configs/routes/index.ts
@@ -3,9 +3,6 @@ import PostRouter from '@controllers/Posts';
 
 const router = Router();
 
-router.get('/posts/userId/:userId', PostRouter.getAllPosts);
-router.post('/posts', PostRouter.create);
-router.put('/posts/update/userId/:id', PostRouter.update);
-router.delete('/posts/delete/:id', PostRouter.delete);
+router.post('/posts/create/:userId', PostRouter.create);
 
 export default router;
