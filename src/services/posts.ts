fd377ed1323f053f99dc2f42207ca251a99d105f
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-02-05 11:46:17 +0700

    su ly trong services thay vi controller

---
+++ src/services/posts.ts
@@ -0,0 +1,37 @@
+import { Model, Sequelize, ModelScopeOptions } from 'sequelize';
+import HastagEntity from '@entities/hastags';
+import HastagInterface from '@interfaces/hashtags';
+import PostModel from './posts';
+
+class HashtagModel extends Model<HastagInterface> implements HastagInterface {
+  public id: number;
+  public name: string;
+
+  static readonly CREATABLE = ['name'];
+
+  static readonly scopes: ModelScopeOptions = {
+    byname(name) {
+      return {
+        where: { name: name },
+      };
+    },
+  };
+
+  public static initialize(sequelize: Sequelize) {
+    this.init(HastagEntity, {
+      tableName: 'hashtags',
+      sequelize,
+      timestamps: false,
+    });
+  }
+
+  public static associate() {
+    HashtagModel.belongsToMany(PostModel, {
+      through: 'PostHashtags',
+      foreignKey: 'hashtagId',
+      as: 'posts',
+    });
+  }
+}
+
+export default HashtagModel;
