ad4c729bea8f4d65841dff98d03fc162a615f861
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-02-06 11:06:29 +0700

    tach logic sang model

--- src/controllers/Posts.ts
+++ src/controllers/Posts.ts
@@ -1,20 +1,20 @@
 import { Request, Response } from 'express';
 import { sendError, sendSuccess } from '@libs/response';
-import PostService from '@services/posts';
+import PostModel from '@models/posts';
 
 class PostController {
   public async create(req: Request, res: Response) {
+    const { userId } = req.params;
+    const { text } = req.fields;
+    const media = req.files?.media;
+
+    if (!media) {
+      return sendError(res, 400, 'Media is required to create post');
+    }
+
     try {
-      const { userId } = req.params;
-      const { text } = req.fields;
-      const media = req.files?.media;
-      if (!media) {
-        return sendError(res, 400, 'Media is required to create post');
-      }
-
-      const newPost = await PostService.createPost(userId, text, media);
+      const newPost = await PostModel.createPost(userId, text, media);
       return sendSuccess(res, newPost, 'Post created successfully');
-
     } catch (error) {
       return sendError(res, 500, 'Error creating post', error.message || error);
     }
