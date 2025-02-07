b8cc04da585cbbe92aaf9d0b054aae32acf5e05a
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2025-02-07 11:50:45 +0700

    can prams

--- src/configs/routes/index.ts
+++ src/configs/routes/index.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express';
 import PostController from '@controllers/Posts';
 const router = Router();
-
-
 router.post('/posts/create/:userId', PostController.create);
-
-
 export default router;
