287db161901ba06beaecdc3c15753f5f7f417eac
Kravys <kiencuongthanh@gmail.com>
2025-02-10 08:56:37 +0700

    Unfollow

--- src/configs/routes/FollowerRoutes.ts
+++ src/configs/routes/FollowerRoutes.ts
@@ -4,5 +4,5 @@ import FollowerController from '@controllers/api/Follower';
 const router = Router();
 
 router.post('/users/:userId/follow', FollowerController.follow);
-
+router.post('/users/:userId/unfollow', FollowerController.follow); 
 export default router;
--- src/controllers/api/Follower.ts
+++ src/controllers/api/Follower.ts
@@ -32,6 +32,25 @@ class FollowerController {
       sendError(res, 500, {errorCode : 131}, error);
     }
   }
+  public async unfollow (req: Request, res: Response) {
+    try {
+      const {followeeId} = req.body;
+      const followerId = req.currentUser['x-user-id'];
+
+      const follow = await FollowerModel.scope([
+        { method: ['byFollowerAndFollowee', followerId, followeeId] },
+      ]).findOne();
+      if(!follow){
+        return sendError(res, 404, NoData.message);
+      }
+      await follow.destroy();
+        return sendSuccess(res, { message : 'unfollowed' });
+    } catch (error) {
+      sendError(res, 500, {errorCode : 132}, error);
+    }
+  }
+  public async getFollowers (req: Request, res: Response) {}
+  public async getFollowing (req: Request, res: Response) {}
 }
 
 export default new FollowerController();
