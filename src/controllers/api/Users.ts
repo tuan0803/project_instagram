1bcc14e0c8d7bd6aa1613f8ff7fb707188a401bd
tuan080321 <nvt08032k1@gmail.com>
2025-02-12 16:52:59 +0700

    lấy danh sach users

---
+++ src/controllers/api/Users.ts
@@ -0,0 +1,27 @@
+import { InternalError, NoData } from '@libs/errors';
+import { sendError, sendSuccess } from '@libs/response';
+import UserModel from '@models/users';
+import { Request, Response } from 'express';
+
+class UserController {
+  public async index (req: Request, res: Response) {
+    try {
+      const { name } = req.query;
+      const scopes: any[] = [];
+      if (name) {
+        scopes.push({ method: ['byName', name] });
+      }
+      const users = await UserModel.scope(scopes).findAndCountAll();
+
+      if (!users.count) {
+        return sendError(res, 404, NoData);
+      }
+
+      return sendSuccess(res, { users: users.rows });
+    } catch (error) {
+      return sendError(res, 500, InternalError, error);
+    }
+  }
+}
+
+export default new UserController();
