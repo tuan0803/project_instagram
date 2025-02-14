90b436c3a8665de3f412ba299d19133df5624824
Kravys <kiencuongthanh@gmail.com>
2025-02-14 11:54:37 +0700

    Lấy danh sách Follower

--- src/configs/routes/FollowerRoutes.ts
+++ src/configs/routes/FollowerRoutes.ts
@@ -5,5 +5,5 @@ const router = Router();
 
 router.post('/users/:userId/follow', FollowerController.follow);
 router.delete('/users/:userId/unfollow', FollowerController.follow); 
-
+router.get('/users/:userId/getFollower', FollowerController.getFollowers);
 export default router;
